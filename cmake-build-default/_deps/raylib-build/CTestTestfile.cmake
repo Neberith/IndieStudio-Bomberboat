1e10146204b72d4ac87c8b5812c6d2bbfec4e03a
Antony Fantapié <antony.fantapie@epitech.eu>
2021-06-28 19:09:45 +0200

    remove cache

--- cmake-build-default/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/antonyftp/Documents/epitech-2.2nd-year/IndieStudio-Bomberboat/cmake-build-default/_deps/raylib-src
-# Build directory: /Users/antonyftp/Documents/epitech-2.2nd-year/IndieStudio-Bomberboat/cmake-build-default/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
